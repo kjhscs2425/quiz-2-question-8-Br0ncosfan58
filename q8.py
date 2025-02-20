e970a855185ff0a59ecdfe2a8f80815695bfcb5d
Br0ncosfan58 <mwasserstein2025@kehillahstudent.org>
2025-02-20 11:27:36 -0800

    q8

--- q8.py
+++ q8.py
@@ -6,5 +6,14 @@ ends with one of these letters: p, t, n
 
 (This should print out 18 words in total)
 """
+import random
 
-# YOUR CODE HERE
+def random_word():
+    first_letters = ["c", "t", "b"]
+    middle_letters = ["a", "o"]
+    last_letters = ["p", "t", "n"]
+    return f"{random.choice(first_letters)}{random.choice(middle_letters)}{random.choice(last_letters)}"
+
+if __name__ == "__main__":
+  for _ in range(18):
+    print(random_word())
